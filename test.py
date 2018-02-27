bdf940e6d583b0977972eba39de6c38496f2ee7d
张愚生 <zhangyusheng@jinkejoy.com>
2018-02-27 20:18:08 +0800

    暂时能跑起来

--- test.py
+++ test.py
@@ -8,26 +8,22 @@ from util.String import *
 def getDetail(code):
     codeList = []
     for item in code:
-        if item[0] == '6':
-            codeList.append(item)
+       # if item[0] == '6':
+        codeList.append(item)
     # 抓取数据并保存到本地csv文件
     print(codeList.__len__())
     for code in codeList:
         html=''
-        print('正在获取股票%s数据' % code)
         url = 'http://quotes.money.163.com/service/chddata.html?code=0' + code + \
          '&end='+String.getNowTime('%Y%m%d')+'&start='+String.getNowTime('%Y%m%d')+'&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;TURNOVER;VOTURNOVER;VATURNOVER;TCAP;MCAP'
-        print(url)
         # url = 'http://stockpage.10jqka.com.cn/'+code+'/'
-        # url='http://quote.eastmoney.com/sz002214.html';
+        # url='http://quote.eastmoney.com/sz002214.html'
+        print(url)
         html = http_html.getHtml(url)
         if str(html).__len__()>90:
           return_str = (str(html).replace("日期,股票代码,名称,收盘价,最高价,最低价,开盘价,前收盘,涨跌额,涨跌幅,换手率,成交量,成交金额,总市值,流通市值",'').replace('\'', '').strip())
-          print(return_str)
           finall = return_str.split(',')
-
-          getSQL(finall)
-          #jdbc.insert(getSQL(finall))
+          jdbc.insert(getSQL(finall))
         # print(html)
         # return html
 
